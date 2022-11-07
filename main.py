339bab1f46cb1f6c92d7b680500e0e43898a78ed
Miroslav Purkrabek <mira.purkrabek@gmail.com>
2022-11-07 09:50:11 +0100

    reading arguments from file added by Microsoft Forms

--- main.py
+++ main.py
@@ -1,3 +1,89 @@
-from hikload.__main__ import main
+import logging
+import logging.config
+import signal
+import sys
+import os
+import yaml
 
-main()
+from win10toast import ToastNotifier
+
+from hikload.download import run, parse_args
+from hikload.__main__ import main_ui
+from onedrive_utils import parse_responses_and_return_latest, argfile_to_argstr, cleanup_old_files
+
+config_path = "logging_config.yml"
+with open(config_path, "r") as f:
+    config_data = yaml.safe_load(f.read())
+    logging.config.dictConfig(config_data)
+logger = logging.getLogger("HikLoadHandler")
+
+# Macros
+ROOT = os.path.dirname(os.path.abspath(__file__))
+CAMERA_TRANSLATION = {
+    "EAST": "101",
+    "SOUTH": "201",
+    "WEST": "301",
+    "NORTH": "401",
+    "TOP": "501",
+}
+
+def main(): 
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    cleanup_old_files()
+    
+    if len(sys.argv) < 2 :
+        arg_str = argfile_to_argstr(parse_responses_and_return_latest())
+        if arg_str is None:
+            logger.info("No new file to process")
+            return
+        else:
+            sys.argv.extend(arg_str.split())
+    args = parse_args()
+
+    # Load default password from the file
+    try:
+        with open(os.path.join("passwords", "passwords.yml"), "r") as pass_file:
+            default_passwords = yaml.safe_load(pass_file)
+    except FileNotFoundError:
+        default_passwords = None
+    
+    if args.server == "" or args.server is None:
+        assert default_passwords is not None, "No server specified and couln't load the passwords.yml file"
+        args.server = default_passwords["HikServer"]["address"]
+    if args.username == ""or args.username is None:
+        assert default_passwords is not None, "No username specified and couln't load the passwords.yml file"
+        args.username = default_passwords["HikServer"]["user"]
+    if args.password == "" or args.password is None:
+        assert default_passwords is not None, "No password specified and couln't load the passwords.yml file"
+        args.password = default_passwords["HikServer"]["password"]
+
+    # Translate cameras
+    if args.cameras is not None:
+        for i, camera in enumerate(args.cameras):
+            if camera.upper() in CAMERA_TRANSLATION.keys():
+                args.cameras[i] = CAMERA_TRANSLATION[camera.upper()]
+
+    # Show notification that the program was executed
+    # toast = ToastNotifier()
+    # toast.show_toast(
+    #     "Download started",
+    #     "Downloading videos from the Hikvision NVR. Cameras: {}".format(args.cameras),
+    #     duration = 3,
+    #     icon_path = "hik.ico"
+    # )
+
+    logger.info("Running the downloading session")
+    logger.info(args)
+
+    try:
+        if args.ui:
+            main_ui(args)
+        else:
+            logging.info("If you want to use the UI, use the --ui flag")
+            run(args)
+    except KeyboardInterrupt:
+        logging.info("Exited by user")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
---
+++ onedrive_utils.py
@@ -0,0 +1,151 @@
+import sys
+import os
+from ast import literal_eval
+import yaml
+from datetime import timedelta, datetime
+
+# Has to be hardcoded because of non-unicode chars
+ONEDRIVE_FOLDER="D:\\OneDrive - TJ Sokol Královské Vinohrady\\HikLoad_commands"
+
+RESPONSE_EXTENSION=".resp"
+ARGUMENTS_EXTENSION=".yml"
+PARSED_KEYWORDS = [
+    "starttime",
+    "endtime", 
+    "cameras",
+    "videoname",
+]
+PROCESS_DEADLINE=timedelta(hours=1)
+DELETE_DEADLINE=timedelta(days=7)
+
+
+def argname_from_response(response_name, response_extension=RESPONSE_EXTENSION, arguments_extension=ARGUMENTS_EXTENSION):
+    dirname, basename = os.path.split(response_name)
+    basename = basename.replace(" ", "_").replace(response_extension, arguments_extension)
+    return os.path.join(dirname, basename)
+
+
+def parse_time(time_str):
+    splitted = time_str.split(":")
+    time = [0, 0, 0]
+    for i, str_s in enumerate(splitted):
+        time[i] = int(str_s)
+    return "{:02d}:{:02d}:{:02d}".format(time[0], time[1], time[2])
+
+
+def parse_cameras(cameras_arr):
+    return ",".join(literal_eval(cameras_arr))
+
+
+def parse_onedrive_response(filepath):
+    with open(filepath, "r") as fl_in:
+        args_dict = {
+            "skipdownload": None,
+            # "concat": None,
+            # "trim": None,
+        }
+        for line in fl_in.readlines():
+            key, value = line.strip().split("?")
+            key = key.lower()
+
+            if key.endswith("time"):
+                date, time = value.split("T")
+                time = parse_time(time)
+                value = "{}T{}".format(date, time)
+            elif key == "cameras":
+                value = parse_cameras(value)
+            elif key == "upload":
+                value = literal_eval(value)
+
+            args_dict[key] = value
+        
+        with open(argname_from_response(filepath), "w") as fl_out:
+            yaml.safe_dump(args_dict, fl_out, indent=2)
+
+
+def parse_responses_and_return_latest(
+    onedrive_folder=ONEDRIVE_FOLDER,
+    extension=RESPONSE_EXTENSION,
+):
+    # Get unparsed responses files
+    responses = []
+    for f in os.listdir(onedrive_folder):
+        f_path = os.path.join(onedrive_folder, f)
+        if os.path.isfile(f_path) and f_path.endswith(extension):
+            responses.append(f_path)
+    
+    responses.sort(key=lambda x: os.path.getmtime(x))
+
+    for response in responses:
+        try:
+            parse_onedrive_response(response)
+            os.remove(response)
+        except:
+            pass
+
+    latest = get_latest_arguments(onedrive_folder)
+    if latest is None:
+        return None
+
+    deadline = datetime.now() - PROCESS_DEADLINE
+    mod_time = datetime.fromtimestamp(os.path.getmtime(latest))
+    if mod_time < deadline:
+        return None
+    else:
+        return latest
+
+
+def get_latest_arguments(
+    folder=ONEDRIVE_FOLDER,
+    arguments_extension=ARGUMENTS_EXTENSION
+):
+    # Sort by modification date (newest first)
+    files = []
+    for f in os.listdir(folder):
+        f_path = os.path.join(folder, f)
+        if os.path.isfile(f_path) and f_path.endswith(arguments_extension):
+            files.append(f_path)
+    files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+    if len(files) == 0:
+        return None
+    else:
+        return files[0]
+
+
+def argfile_to_argstr(filepath, arguments_extension=ARGUMENTS_EXTENSION):
+    if filepath is None:
+        return None
+    assert filepath.endswith(arguments_extension)
+
+    out_str = ""
+    with open(filepath, "r") as fl:
+        args_dict = yaml.safe_load(fl)
+        for key, value in args_dict.items():
+            key = key.lower()
+            if key in PARSED_KEYWORDS:
+                if key == "cameras":
+                    out_str += "--{}={} ".format(key, value)
+                else:
+                    out_str += "--{} {} ".format(key, value)
+            elif value is None:
+                out_str += "--{} ".format(key)
+
+    return out_str
+
+
+def cleanup_old_files(
+    folder=ONEDRIVE_FOLDER,
+    deadline=DELETE_DEADLINE
+):
+    for f in os.listdir(folder):
+        f_path = os.path.join(folder, f)
+        if os.path.isfile(f_path):
+            mod_time = datetime.fromtimestamp(os.path.getmtime(f_path))
+            if mod_time < (datetime.now() - deadline):
+                os.remove(f_path) 
+
+
+if __name__ == "__main__":
+    latest = parse_responses_and_return_latest()
+    print(argfile_to_argstr(latest))
+    
